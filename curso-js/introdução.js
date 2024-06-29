ff2438263870f1254f9a21add1c5934c9ef4f68c
VINICIUS SILVA <163214766+viniciussilvadaconceicao@users.noreply.github.com>
2024-06-29 12:43:53 -0300

    Add files via upload

---
+++ curso-js/introdução.js
@@ -0,0 +1,8 @@
+//aula 1 //
+
+document.querySelector('h1.content-head__title').innerText='Vinicius é a grande promessa no mundo da programação'
+
+document.querySelector('h2.content-head__subtitle').innerText=
+'nascido em duque de caxias, Rio dejaneiro e criado na cidade de Deus e Saquarema, ele se esforça para ser o top 1 do Rio de janeiro '
+
+//questão 1//
