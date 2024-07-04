0975945d1c84aad0d1ce6542343e115a529404ab
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-04 16:43:39 -0300

    Update condiçoesJS.js

--- Curso em video - JS/condiçoesJS.js
+++ Curso em video - JS/condiçoesJS.js
@@ -1,1 +1,10 @@
 console.log('Condições em JS')
+
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 60){//condição simples
+    console.log('você ultrapassou o limite de velocidade.MULTADO!')
+}
+
+console.log('Dirija sempre com cinto de segurança!')
