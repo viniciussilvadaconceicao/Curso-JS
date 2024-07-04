7b1eeb151973150fa371acebda4642797c485423
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-04 17:04:11 -0300

    aula11

--- Curso em video - JS/ex009.js
+++ Curso em video - JS/ex009.js
@@ -2,8 +2,8 @@ var pais = 'EUA'
 console.log(`Vivendo em país ${pais}`)
 
 if (pais == 'Brasil'){
-    console.log('Você é brasileiro')
+    console.log('Você é brasileiro!')
 }
 else{
-    console.log('Você é estrangeiro')
+    console.log('Você é estrangeiro!')
 }
