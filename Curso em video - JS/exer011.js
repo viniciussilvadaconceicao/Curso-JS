b9bf55a4014f481032ab827abb6b693ac9237269
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-05 15:26:42 -0300

    aninhamentoJS

---
+++ Curso em video - JS/exer011.js
@@ -0,0 +1,14 @@
+var idade = 65
+console.log(`Você tem ${idade} anos de idade`) 
+if (idade < 16) {
+    console.log('Não vota')
+} 
+
+else if (idade < 18|| idade >= 65) {
+    console.log('Vota se quiser');
+}
+
+else{
+    console.log('voto obrigatório');
+    }
+
