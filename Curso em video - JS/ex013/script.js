57dcc1ea1369320e62d727bc3f328c90b4fe7db5
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-09 12:26:46 -0300

    Upgrade

---
+++ Curso em video - JS/ex013/script.js
@@ -0,0 +1,27 @@
+function Verificar(){
+    var agora = new Date()
+    var hora = agora.getHours()
+    var resp = window.document.getElementById('resp')
+    var fot = window.document.getElementById('fot')
+    
+
+    if(hora >= 0 && hora < 12){
+        fot.innerHTML = '<img id="imagem" src="imagem/manha.jpg" alt="foto do dia"></img>'
+        resp.innerHTML = `Agora são ${hora} horas. Bom dia!`
+        document.body.style.background = 'yellow'
+        document.body.style.color = 'black'
+        
+    }
+    else if(hora >= 12 && hora < 18){
+        fot.innerHTML = ' <img src="imagem/tarde.jpg" alt="foto da tarde"></img>'
+        resp.innerHTML = `Agora são ${hora} horas. Boa tarde!`
+        document.body.style.background = 'orange'
+        document.body.style.color = 'black'
+    }
+    else{
+        fot.innerHTML = '<img src="imagem/noite.jpg" alt="foto da noite"></img></img>'
+        resp.innerHTML = `Agora são ${hora} horas. Boa noite!`
+        document.body.style.background = 'black'
+        document.body.style.color = 'white'
+    }
+}
