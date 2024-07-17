f0ff10845f73d7b42cf3335e7961e606bccc4f98
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-17 17:44:27 -0300

    for

---
+++ exerciciosJS/repetiçoes-For.js
@@ -0,0 +1,5 @@
+console.log(`vai começar ...`)
+for (var cont = 1;cont<=10;cont++){
+    console.log(`passo ${cont}`)
+}
+console.log('FIM!')
--- exerciciosJS/repetiçoes-while.js
+++ exerciciosJS/repetiçoes-while.js
@@ -2,7 +2,7 @@ var cont = 1
 do {
     console.log(` passo ${cont}`)
     cont++
-} while (cont <= 6)
+} while (cont <= 6)// teste logico no final
 
 /*var cont = 1
 while(cont <= 6){
