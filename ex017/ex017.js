b1f69ad2f34847a8c1276c04cecf7d74ff450dab
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-18 11:41:23 -0300

    condiçoes

--- ex017/ex017.js
+++ ex017/ex017.js
@@ -5,6 +5,15 @@ function geratabela(){
     var passo = 1
     var rsp = document.getElementById('rsp')
     for (var cont = inicio; cont <= final; cont += passo){
-        rsp.innerHTML += `${valor.value} x ${cont} = ${valor.value * cont} <br>`
+        if (valor.value == ''){
+            alert('Digite um valor na caixa de texto')
+            rsp.innerHTML = 'Impossível gerar a tabuada!'
+            return;
+        } else if(valor.value <= 0){
+            alert('digite valores positivos')
+            rsp.innerHTML = 'Impossível gerar a tabuada com numeros negativos!'
+        } else if (valor.value > 0){
+            rsp.innerHTML += `${valor.value} X ${cont} = ${valor.value * cont} <br>`
+        }
     }
 }
