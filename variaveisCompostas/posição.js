6e9c3223934aefcfb305fba1508314ccf4112224
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-18 21:05:55 -0300

    JS indexOf

---
+++ variaveisCompostas/posição.js
@@ -0,0 +1,9 @@
+// Objetivo: Encontrar a posição de um valor em um vetor
+valores = [5, 8, 2, 9, 3, 7]
+let pos = valores.indexOf(7)
+if (pos == -1){
+    console.log(`O valor não foi encontrado`)
+}else{
+    console.log(`O valor está na posição ${pos}`)
+}
+    
