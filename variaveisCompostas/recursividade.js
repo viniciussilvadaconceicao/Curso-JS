b287200b80624d81ce3278668f888fb2bca6a14e
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-20 16:39:14 -0300

    Create recursividade.js

---
+++ variaveisCompostas/recursividade.js
@@ -0,0 +1,13 @@
+/* 5! = 5 x 4 x 3 x 2 x 1 
+4! = 4 x 3 x 2 x 1
+1! = 1*/
+function fatorial(n){
+    if (n == 1){
+        return 1
+    } else{
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(5))
+// 5 x 4 x 3 x 2 x 1 = 120
