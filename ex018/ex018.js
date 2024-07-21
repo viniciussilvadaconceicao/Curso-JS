59bfd908e753280f1fa163e31f688d19c286b56f
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-21 11:41:49 -0300

    arry

--- ex018/ex018.js
+++ ex018/ex018.js
@@ -1,13 +1,28 @@
-function adicionar(){
+function adicionar() {
     let numero = document.getElementById('numero').value;
     let selnum = document.getElementById('selnum');
-    if (numero === ''){
+    
+    if (numero === '') {
         alert('Por favor, digite um número!');
-    } else if(numero < 1 || numero > 100){
+    } else if (numero < 1 || numero > 100) {
         alert('Por favor, digite um número entre 1 e 100!');
     } else {
-        let item = document.createElement('option');
-        item.innerHTML = `Valor ${numero} adicionado.`;
-        selnum.appendChild(item);
+        let optionExists = false;
+        
+        for (let i = 0; i < selnum.options.length; i++) {
+            if (numero === selnum.options[i].value) {
+                optionExists = true;
+                break;
+            }
+        }
+        
+        if (optionExists) {
+            alert('Número já adicionado!');
+        } else {
+            let item = document.createElement('option');
+            item.text = `Valor ${numero} adicionado.`;
+            item.value = numero; // Add this line to set the value of the option
+            selnum.appendChild(item);
+        }
     }
 }
